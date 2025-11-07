7e890c58939f2d535503ee820967f48c63cca8c6
Felipinho1806 <fcnfelipe04@gmail.com>
2025-11-07 15:34:35 -0300

    Resolução de bug com relação a dinamicidade das datas na tela inicial

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
 import 'modules/onboarding/view/onboarding_screen.dart';
 import 'modules/onboarding/view/login.dart';
+import 'package:intl/date_symbol_data_local.dart'; 
 
-void main() {
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  await initializeDateFormatting('pt_BR', null); 
+  
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+ const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Daily XP',
-      initialRoute: '/onboarding',
-      routes: {
-        '/onboarding': (context) => const OnboardingScreen(),
-        '/login': (context) => const LoginScreen(),
-      },
-    );
-  }
+ @override
+ Widget build(BuildContext context) {
+ return MaterialApp(
+ debugShowCheckedModeBanner: false,
+ title: 'Daily XP',
+ initialRoute: '/onboarding',
+ routes: {
+ '/onboarding': (context) => const OnboardingScreen(),
+ '/login': (context) => const LoginScreen(),
+ },
+ );
+}
 }
